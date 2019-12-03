c000ec4b3920c4e496e02089c4dc0768763491c3
wangdongshan <wangdongshan@id.link>
2019-12-03 17:27:02 +0800

    create-package

---
+++ src/algorithm/easy/twoSum.java
@@ -0,0 +1,8 @@
+package algorithm.easy;
+
+/**
+ * @author dongshan
+ * @create 2019-12-03 17:24
+ */
+public class twoSum {
+}
---
+++ src/solution.java
@@ -0,0 +1,6 @@
+/**
+ * @author dongshan
+ * @create 2019-12-03 17:24
+ */
+public class solution {
+}
