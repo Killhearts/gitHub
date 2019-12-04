83bf743ac811815f1045e52417b3f64be5f49f34
wangdongshan <wangdongshan@id.link>
2019-12-04 17:41:51 +0800

    romanToINT

--- src/algorithm/easy/reverseInt.java
+++ src/algorithm/easy/reverseInt.java
@@ -5,12 +5,16 @@ package algorithm.easy;
  * @create 2019-12-04 10:45
  */
 public class reverseInt {
-    public static int reverseInt(int x) {
+    public static int reverseInt1(int x) {
         int re = 0;
         while(x!=0){
-            re = re*10+x%10;
+            int pop = x % 10;
             x/=10;
+            if (re > Integer.MAX_VALUE/10 || (re == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (re < Integer.MIN_VALUE/10 || (re == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            re = re*10+pop;
+
         }
-        return (re>Integer.MAX_VALUE || re<Integer.MIN_VALUE) ? 0 :re;
+        return re;
     }
 }
--- src/algorithm/easy/solution.java
+++ src/algorithm/easy/solution.java
@@ -11,9 +11,12 @@ public class solution {
         //两数之和
         //System.out.println(Arrays.toString(twoSum.twoSum2(new int[]{2,7,11,16},18)));
         //整数翻转
-        //System.out.println(reverseInt.reverseInt(120));
+        System.out.println(reverseInt.reverseInt1(1534236469));
         //是否回文
-        System.out.println(isPalindrome.isPalindrome2(1221));
+        //System.out.println(isPalindrome.isPalindrome2(1221));
+        //转罗马数字
+        System.out.println(romanToInt.romanToInt("LVIII"));
+        System.out.println(romanToInt.romanToInt1("LVIII"));
 
     }
 }
